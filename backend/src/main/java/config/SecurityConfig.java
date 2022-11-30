3a6a1e34ba12586bcb77130ba13b0cba409c15e4
mbnaysinger <maikenborges@gmail.com>
2022-11-29 21:45:20 -0300

    Banco de Dados H2

--- backend/src/main/java/config/SecurityConfig.java
+++ backend/src/main/java/config/SecurityConfig.java
@@ -5,12 +5,15 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.transaction.TransactionDefinition.withDefaults;
 
 @Configuration
 @EnableWebSecurity
